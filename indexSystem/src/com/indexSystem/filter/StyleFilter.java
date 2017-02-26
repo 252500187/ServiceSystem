36aaa2ca55e1c189ed7a5f9e1beac407879d099e
252500187@qq.com <19930422>
2017-02-26 22:50:47 +0800

    commit

--- indexSystem/src/com/indexSystem/filter/StyleFilter.java
+++ indexSystem/src/com/indexSystem/filter/StyleFilter.java
@@ -1,8 +1,21 @@
 package com.indexSystem.filter;
 
-import javax.servlet.Filter;
+import javax.servlet.*;
+import java.io.IOException;
+
 /**
  * Created by Noce_ on 2017/2/26.
  */
 public class StyleFilter implements Filter{
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
 }
