8b42b1cc11d494e27db47209a978408148bd68a1
lijunbo <252500187@qq.com>
2017-02-27 19:47:42 +0800

    commit

--- indexSystem/src/com/indexSystem/dao/impl/UserDaoImpl.java
+++ indexSystem/src/com/indexSystem/dao/impl/UserDaoImpl.java
@@ -2,12 +2,20 @@ package com.indexSystem.dao.impl;
 
 import com.indexSystem.dao.UserDao;
 import com.indexSystem.vo.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.swing.tree.RowMapper;
 
 /**
  * Created by lijunbo on 2017/2/27.
  */
-public class UserDaoImpl implements UserDao{
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     public UserInfo getUserInfo(String userName) {
-        return null;
+        return jdbcTemplate.queryForObject("SELECT * FROM user_info WHERE user_name = ?", UserInfo.class, userName);
     }
 }
